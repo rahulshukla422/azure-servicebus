ce5f50056fa0848e1b8a14427ac237f15441c542
Rahul Shukla <rahulshukla422@gmail.com>
2020-06-11 01:08:01 +0530

    azure service bus .net code

---
+++ azure-servicebus-listener/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace azure_servicebus_reciever
+{
+    class Program
+    {
+        const string servicebusConenctionString = "Endpoint=sb://service-bus-temp.servicebus.windows.net/;SharedAccessKeyName=listen-keys;SharedAccessKey=xrYg904qU0KRTJe2gTSySgzG9QEmcwyrZZWXyNaJdNU=;";
+        const string topicName = "topic-temp";
+        const string subcriptionName = "topic-sub";
+        private static ISubscriptionClient subscriptionClient;
+        static async Task Main()
+        {
+            Console.WriteLine("Receiving the message from service bus topic...");
+
+            subscriptionClient = new SubscriptionClient(servicebusConenctionString, topicName, subcriptionName);
+
+            // Register the message handler and recieve the messages in the loop
+            RegisterMessageHander();
+
+            Console.ReadKey();
+            await subscriptionClient.CloseAsync();
+        }
+
+        private static void RegisterMessageHander()
+        {
+            MessageHandlerOptions messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            //Register the function to process the message
+            subscriptionClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+
+        }
+
+        private static async Task ProcessMessageAsync(Message msg, CancellationToken arg2)
+        {
+            var message = Encoding.UTF8.GetString(msg.Body);
+
+            var response = JsonConvert.DeserializeObject<System.Collections.Generic.List<Order>>(message);
+
+            foreach (var item in response)
+            {
+                Console.WriteLine($"{item.Id}  {item.Name}  {item.Amount}");
+            }
+
+            await subscriptionClient.CompleteAsync(msg.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message handler encountered an exception {arg.Exception}.");
+            var context = arg.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Amount { get; set; }
+
+    }
+}
---
+++ azure-servicebus-sender/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace azure_servicebus
+{
+    class Program
+    {
+        const string servicebusConnectionString = "Endpoint=sb://service-bus-temp.servicebus.windows.net/;SharedAccessKeyName=send-keys;SharedAccessKey=CgxmRWRE7eCmU7EnzQBbd4EgiCADepim0cs5Jjl8mRQ=;";
+
+        const string topicName = "topic-temp";
+
+        private static ITopicClient topicClient;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Sending message to the topic");
+
+            topicClient = new TopicClient(servicebusConnectionString, topicName);
+
+            await SendMessage();
+
+            Console.WriteLine("Message has been sent!");
+
+            Console.ReadKey();
+
+        }
+
+        private static async Task SendMessage()
+        {
+            var serializedData = JsonConvert.SerializeObject(GetSampleMessage());
+            Message message = new Message(Encoding.UTF8.GetBytes(serializedData));
+
+            await topicClient.SendAsync(message);
+        }
+
+        private static List<Order> GetSampleMessage()
+        {
+            var list = new List<Order>()
+            {
+                new Order
+                {
+                    Id =1,
+                    Name ="Burger",
+                    Amount =1000,
+                },
+                new Order
+                {
+                    Id =1,
+                    Name ="Pizza",
+                    Amount =1200,
+                },
+                new Order
+                {
+                    Id =1,
+                    Name ="Sandwich",
+                    Amount =1100,
+                }
+            };
+
+            return list;
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Amount { get; set; }
+
+    }
+}
